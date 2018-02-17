c8e30158504d777ff02c81371a2099a7c5584608
lifez <lifez999@gmail.com>
2018-02-17 14:55:11 +0700

    Initial commit

---
+++ tc-plugin/transient-demo.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Plugin Name: Transient Demo
+ * Plugin URI: http://www.prontotools.io
+ * Description: A suite of gallery features
+ * Version: 0.0.1
+ * Author: Pronto Tools
+ * Author URI: http://www.prontotools.io
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
