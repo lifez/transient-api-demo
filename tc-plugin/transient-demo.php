9a2d3d7536246cbec0c17a855844158fd113861a
lifez <lifez999@gmail.com>
2018-02-17 16:13:48 +0700

    Add plugin

--- tc-plugin/transient-demo.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Plugin Name: Transient Demo
- * Description: Transient Demo
- * Version: 0.0.1
- * License: GNU General Public License v3.0
- * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- */
-
---
+++ transient-demo/transient-demo.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Plugin Name: Transient Demo
+ * Description: Transient Demo
+ * Version: 0.0.1
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+function render_feed() {
+    if ( $content = get_transient( 'transient-feed' ) ) {
+        return $content;
+    }else {
+        return generate_content();
+    }
+}
+
+function generate_content() {
+    $args = array( 'posts_per_page' => 10 );
+    $posts = get_posts( $args );
+
+    $content = '';
+    foreach ( $posts as $post ) {
+        $content .= '<p>' . $post->post_title . '</p>';
+    }
+    set_transient( 'transient-feed', $content, 5 );
+    return $content;
+}
+
+add_shortcode( 'transient-demo', render_feed );
+
+add_action( 'save_post', 'generate_content' );
+add_action( 'delete_post', 'generate_content' );
