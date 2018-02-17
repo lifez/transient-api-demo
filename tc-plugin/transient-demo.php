6a4fe355ccfa84377ca1b4c3ebc96bccf5e7fcca
lifez <lifez999@gmail.com>
2018-02-17 14:55:46 +0700

    Change description

--- tc-plugin/transient-demo.php
+++ tc-plugin/transient-demo.php
@@ -1,11 +1,8 @@
 <?php
 /*
  * Plugin Name: Transient Demo
- * Plugin URI: http://www.prontotools.io
- * Description: A suite of gallery features
+ * Description: Transient Demo
  * Version: 0.0.1
- * Author: Pronto Tools
- * Author URI: http://www.prontotools.io
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
